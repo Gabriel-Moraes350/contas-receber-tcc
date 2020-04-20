ef2aafeb037c5b20aa0ffbef556cdf071240d0dc
Gabriel Moraes <gabriel.m.baptista@gmail.com>
2020-04-20 00:24:59 -0300

    fix testing

---
+++ src/test/kotlin/com/techeasy/contas/receber/InitTest.kt
@@ -0,0 +1,19 @@
+package com.techeasy.contas.receber
+
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class InitTest {
+
+	@Test
+	fun testIfTrue() {
+		assertTrue(true)
+	}
+
+	@Test
+	fun testIfFalse() {
+		assertFalse(false)
+	}
+
+}
